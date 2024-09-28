0b80ea6dcc1fcfa60359d94af1fcfb499a7d084e
Chamindu Indula <chamibusiness3@gmail.com>
2024-09-28 19:17:29 +0530

    ❤️Queen Tharu❤️

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
-ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/N9sJX9Q/IMG-20240923-WA0005.jpg",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/YhYStZ8/IMG-20240923-WA0006.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "HII. I AM QUEEN THARU WHATSAPP BOT OWNER BY CHAMI",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "ture",
 };
